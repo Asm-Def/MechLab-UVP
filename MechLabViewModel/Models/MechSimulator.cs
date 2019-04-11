b88ed5c796c7042fcb4274bdd1f9a47a9b653b90
Asm-Def <willywu@foxmail.com>
2019-04-11 21:17:00 +0800

    在MechSimulator中新增_objects数组用于维护所有物理模型

--- MechLabViewModel/Models/MechSimulator.cs
+++ MechLabViewModel/Models/MechSimulator.cs
@@ -6,5 +6,21 @@ namespace MechLabLibrary.Models
 {
     public class MechSimulator
     {
+        MechObject[] _objects;
+        /// <summary>
+        /// 当Running=true时持续对_objects进行模拟
+        /// </summary>
+        bool Running = false;
+        /// <summary>
+        /// 应在程序开始时另开一个线程执行
+        /// </summary>
+        void Simulate()
+        {
+            while(true)
+            {
+                if (!Running) continue;
+
+            }
+        }
     }
 }
