ab8938ff3e463bd35db840db19349a174e3d6493
Haizs Chen <me@haizs.net>
2019-05-24 14:20:02 +0800

    Add converters

---
+++ MechLab-UVP/Converters/ByteToBitmapImageConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace MechLab_UVP.Converters
+{
+    public class ByteToBitmapImageConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            byte[] imageBytes = (byte[]) value;
+            return ConvertByteToImage(imageBytes).Result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task<BitmapImage> ConvertByteToImage(byte[] imageBytes)
+        {
+            BitmapImage image = new BitmapImage();
+            using (var randomAccessStream = new InMemoryRandomAccessStream())
+            {
+                using (var writer = new DataWriter(randomAccessStream.GetOutputStreamAt(0)))
+                {
+                    writer.WriteBytes(imageBytes);
+                    await writer.StoreAsync();
+                    await image.SetSourceAsync(randomAccessStream);
+                }
+            }
+            return image;
+        }
+    }
+}
---
+++ MechLab-UVP/Converters/StringToBoolConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace MechLab_UVP.Converters
+{
+    public class StringToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return !string.IsNullOrEmpty(value?.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
